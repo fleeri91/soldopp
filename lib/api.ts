8ddf0fca46bb8ce7fb6dfe6e9404ba067a43c01e
Flemming Eriksson <fleeri91@gmail.com>
2026-05-17 20:34:19 +0200

    api

---
+++ lib/api.ts
@@ -0,0 +1,64 @@
+import { BathingWaterProfile } from "@/types/BathingWaters/BathingWaterProfile";
+import { BathingWaters } from "@/types/BathingWaters/BathingWaters";
+import { PointForecast } from "@/types/SMHI/PointForecast";
+import { WeatherParameter } from "@/types/SMHI/WeatherParameters";
+
+const SMHI_BASE =
+  "https://opendata-download-metfcst.smhi.se/api/category/snow1g/version/1/geotype/point";
+
+export const getPointForecast = async (
+  lat: number,
+  lon: number,
+  parameters?: WeatherParameter[],
+  timeseries?: number,
+): Promise<PointForecast> => {
+  const queryParams = new URLSearchParams();
+
+  if (timeseries !== undefined) {
+    queryParams.append("timeseries", timeseries.toString());
+  }
+
+  if (parameters && parameters.length > 0) {
+    queryParams.append("parameters", parameters.join(","));
+  }
+
+  const queryString = queryParams.toString();
+  const url = `${SMHI_BASE}/lon/${lon.toFixed(6)}/lat/${lat.toFixed(6)}/data.json${queryString ? `?${queryString}` : ""}`;
+
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    throw new Error(`SMHI response was not ok: ${response.statusText}`);
+  }
+
+  const data: PointForecast = await response.json();
+  return data;
+};
+
+export const getBathingWaters = async (): Promise<BathingWaters> => {
+  const response = await fetch(
+    `${process.env.EXPO_PUBLIC_HAV_API_URL}/bathing-waters`,
+  );
+
+  if (!response.ok) {
+    throw new Error(`Network response was not ok: ${response.statusText}`);
+  }
+
+  const data: BathingWaters = await response.json();
+  return data;
+};
+
+export const getBathingWaterProfile = async (
+  id: string,
+): Promise<BathingWaterProfile> => {
+  const response = await fetch(
+    `${process.env.EXPO_PUBLIC_HAV_API_URL}/bathing-waters/${id}/profiles`,
+  );
+
+  if (!response.ok) {
+    throw new Error(`Network response was not ok: ${response.statusText}`);
+  }
+
+  const data: BathingWaterProfile = await response.json();
+  return data;
+};
